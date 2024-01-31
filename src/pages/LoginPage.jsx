24aa0a5655771d002e5c54c778b8a4bdbfdd323f
pablopaul01 <juanpablosalomon@gmail.com>
2024-01-31 18:44:42 -0300

    se agrega el login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,13 +5,13 @@ const LoginPage = ({setIsLogged}) => {
     return (
         <div className="container-fluid shadow login">
             <div className="row justify-content-center row-login">
-                <div className="col-lg-5 col-md-7 col-sm-9 col-xs-10 px-5 shadow div-login">
-                    <div className="titulo border border-top-0 border-end-0 border-start-0">
-                        <div className='my-5 ms-5'>
-                            <h2 className="titulo-login">Iniciar Sesión</h2>
+                <div className="col-lg-4 col-md-7 col-sm-9 col-xs-10 px-5 shadow div-login">
+                    <div className="py-5 titulo border border-top-0 border-end-0 border-start-0 d-flex justify-content-center align-items-center">
+                        <div className='col-6 d-flex justify-content-center align-items-center'>
+                            <h2 className="titulo-login text-center mb-0">Iniciar Sesión</h2>
                         </div>
-                        <div className='d-none d-md-block d-lg-block'>
-                            <img src='https://res.cloudinary.com/dcv6aut2v/image/upload/v1697100801/RollingHotel-transparente_qah5k2.png' className="ms-5 img-fluid w-75" alt="logo-Rolling-Hotel" />
+                        <div className='d-none d-md-block col-6 text-center'>
+                            <img src='https://res.cloudinary.com/dtkrptodh/image/upload/v1706725838/Medias/logo-V.png' className="img-fluid" alt="logo-Rolling-Hotel" style={{width:"70%"}}/>
                         </div>
                     </div>
                     <Login setIsLogged={setIsLogged}/>
